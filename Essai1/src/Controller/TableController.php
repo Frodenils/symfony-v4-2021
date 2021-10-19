e90ed46ef7cb48dd11be59140858f660219227bd
FRODEDELAFORET Nils <frodnil@ndlp.fr>
2021-10-19 16:05:07 +0200

    form

--- Essai1/src/Controller/TableController.php
+++ Essai1/src/Controller/TableController.php
@@ -2,23 +2,51 @@
 
 namespace App\Controller;
 
+use App\Form\TableChoiceType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-    /**
-     * @Route("/table", name="table")
-     */
+
+/**
+ * @Route("/table", name="table")
+ */
 class TableController extends AbstractController
 {
     /**
+     * @Route("/select", name="table_select")
+     */
+    public function select(Request $request)
+    {
+        $form = $this->createForm(TableChoiceType::class);
+        if ($form->isSubmitted()) {
+            $data = $form->getData();
+            $n = $data['table_number'];
+            $m = $data['lines_count'];
+            $color = $data['color'];
+            $response = $this->render('table/index.html.twig', [
+                'controller_name' => 'TableController',
+                'n' => $n,
+                'm' => $m,
+                'color' => $color,
+            ]);
+        } else {
+            return $this->render('table/vue.html.twig', [
+                'formulaire' => $form->createView(),
+            ]);
+        }
+    }
+    /**
      * @Route("/print/{n}/{m}", name="table_print")
      */
-    public function index(int $n, int $m): Response
+    public function index(int $n, int $m, Request $request): Response
     {
+        $color = $request->get('c');
         return $this->render('table/index.html.twig', [
             'controller_name' => 'TableController',
             'n' => $n,
             'm' => $m,
+            'color' => $color,
         ]);
     }
 }
